5bcc3297ded66bf063f0232d68c0834de57e4fe3
ornelijam1 <99713494+ornelijam1@users.noreply.github.com>
2023-05-02 12:39:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log(diena)
 const input1 = document.querySelector('.js-teksts1');
 const input2 = document.querySelector('.js-teksts2');
 const plusButton = document.querySelector('.js-plus');
-const minusButtoon = document.querySelector('.js-minus');
+const minusButton = document.querySelector('.js-minus');
 const multiplyButton = document.querySelector('.js-multiply');
 const divideButton = document.querySelector('.js-divide');
 const result = document.querySelector('.js-result');
@@ -16,17 +16,52 @@ const Add = (skaitlis1, skaitlis2) => {
 }
 
 const Subtract = (skaitlis1, skaitlis2) => {
-    return skaitlis1 - skaitlis2
+    return Number(skaitlis1) - Number(skaitlis2)
 }
 
 const Multiply = (skaitlis1, skaitlis2) => {
-    return skaitlis1 * skaitlis2
+    return Number(skaitlis1) * Number(skaitlis2)
 }
 
 const Divide = (skaitlis1, skaitlis2) => {
-    return skaitlis1 / skaitlis2
+    if(Number(skaitlis2) === 0){
+        alert('Nevar dalīt ar 0')
+        return 'Nav atbildes'
+    }
+
+    return Number(skaitlis1) / Number(skaitlis2)
+}
+
+const resetValues = () => {
+    input1.value = ""
+    input2.value = ""
 }
 
 plusButton.addEventListener('click', () => {
-    result.innerHTML = Add(Number(input1.value), Number(input2.value))
+    result.innerHTML = Add(input1.value, input2.value)
+
+    resetValues();
 })
+
+minusButton.addEventListener('click', () => {
+    result.innerHTML = Subtract(input1.value, input2.value)
+
+    resetValues();
+})
+
+multiplyButton.addEventListener('click', () => {
+    result.innerHTML = Multiply(input1.value, input2.value)
+
+    resetValues();
+})
+
+divideButton.addEventListener('click', () => {
+    result.innerHTML = Divide(input1.value, input2.value)
+
+    resetValues();
+})
+
+
+
+
+
